edbe1f5f2031fee65fd96a436721da3b6d672cef
yohan-wdmr <yohan.wdmr@eduge.ch>
2026-09-03 10:15:31 +0200

    index.php de base

---
+++ horaire-eleve/index.php
@@ -0,0 +1,13 @@
+<?php require_once __DIR__ . '/includes/header.php'; ?>
+
+<h1>Gestion des horaires du CFPT</h1>
+
+<p>Bienvenue dans l'application de gestion des horaires.</p>
+
+<ul>
+    <li><a href="pages/classes.php">Gérer les classes</a></li>
+    <li><a href="pages/cours.php">Gérer les cours</a></li>
+    <li><a href="pages/horaire.php">Gérer les horaires</a></li>
+</ul>
+
+<?php require_once __DIR__ . '/includes/footer.php'; ?>
